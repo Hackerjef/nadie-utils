2a6d17e622db4fba948881bc6c7950de69f95298
Nadie <hackerjef@users.noreply.github.com>
2021-11-04 18:52:12 -0400

    hopefully this works?

--- nautils/plugins/a.py
+++ nautils/plugins/a.py
@@ -8,15 +8,16 @@ class aPlugin(naPlugin):
         super(aPlugin, self).load(ctx)
 
     @naPlugin.listen('MessageCreate')
-    def a_channel_listener(self, event):
+    def a_listener(self, event):
+        self.log.info("f")
         if Getcfgvalue('options.a.enabled', False):
             return
-
+        self.log.info("u")
         if event.channel.id != Getcfgvalue('options.a.cid', None):
             return
-
+        self.log.info("c")
         if event.author.id in Getcfgvalue('options.a.exception', []):
             return
-
+        self.log.info("k")
         if 'a' != event.content:
             event.delete()
