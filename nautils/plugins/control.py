09f2487a0ccf1a83227343641c985ecd199b5a28
Nadie <hackerjef@users.noreply.github.com>
2021-11-03 21:01:33 -0400

    update n stuff

---
+++ nautils/plugins/control.py
@@ -0,0 +1,60 @@
+import os
+import signal
+import git
+
+from disco.bot import CommandLevels
+from gevent.exceptions import BlockingSwitchOutError
+
+from nautils import naPlugin
+
+PY_CODE_BLOCK = u'```py\n{}\n```'
+
+
+class ControlPlugin(naPlugin):
+    def load(self, ctx):
+        super(ControlPlugin, self).load(ctx)
+        # register Process listeners
+        signal.signal(signal.SIGINT, self.ProcessControl)
+        signal.signal(signal.SIGTERM, self.ProcessControl)
+        signal.signal(signal.SIGUSR1, self.ProcessControl)
+
+    @naPlugin.command('update', level=CommandLevels.OWNER)
+    def update(self, event):
+        # thank dooley :bongo:
+        g = git.cmd.Git(os.getcwd())
+        p = g.pull()
+        event.msg.reply(PY_CODE_BLOCK.format(p))
+        self.ProcessControl(signalNumber=2)
+
+    def ProcessControl(self, signalNumber=None, frame=None):
+        if signalNumber == 2:
+            self.log.warning("Graceful shutdown initiated")
+            self.client.gw.shutting_down = True
+            self.client.gw.ws.close(status=4000)
+            self.client.gw.ws_event.set()
+            # unload plugins
+            for x in list(self.bot.plugins):
+                if x == 'CorePlugin' or x == 'ControlPlugin':
+                    self.log.info('Skiping plugin: {}'.format(x))
+                    continue
+                plugin = next((v for k, v in self.bot.plugins.items() if k.lower() == x.lower()), None)
+                if plugin:
+                    self.log.info('Unloading plugin: {}'.format(x))
+                    try:
+                        self.bot.rmv_plugin(plugin)
+                    except BlockingSwitchOutError:
+                        self.log.warning("Plugin {} Has a active greenlet/schedule, Bruteforce".format(x))
+                        # Due to the never ending disco bugs, Just remove the listeners to stop plugin execution after unload
+                        plugin.greenlet = []
+                        plugin.schedule = []
+                        for listener in plugin.listeners:
+                            listener.remove()
+                        pass
+                    except Exception:
+                        self.log.exception("Failed to unload: {}".format(x))
+        elif signalNumber == 10:  # sysuser1
+            self.log.warning("Reseting shard connection to Discord")
+            self.client.gw.ws.close(status=4000)
+        else:
+            self.log.warning("Force Shutdown initiated")
+            os.kill(os.getpid(), signal.SIGKILL)
--- nautils/plugins/core.py
+++ nautils/plugins/core.py
@@ -8,7 +8,6 @@ from disco.bot import CommandLevels
 from disco.bot.command import CommandEvent
 from disco.types.channel import ChannelType
 from disco.types.permissions import Permissions
-from gevent.exceptions import BlockingSwitchOutError
 
 from nautils import naPlugin
 from nautils.config import GetValue
@@ -21,10 +20,6 @@ PY_CODE_BLOCK = u'```py\n{}\n```'
 class CorePlugin(naPlugin):
     def load(self, ctx):
         super(CorePlugin, self).load(ctx)
-        # register Process listeners
-        signal.signal(signal.SIGINT, self.ProcessControl)
-        signal.signal(signal.SIGTERM, self.ProcessControl)
-        signal.signal(signal.SIGUSR1, self.ProcessControl)
 
         # noinspection PyAttributeOutsideInit
         self.startup = ctx.get('startup', datetime.utcnow())
@@ -112,36 +107,3 @@ class CorePlugin(naPlugin):
             event.msg.reply('', attachments=[('result.txt', result)])
         else:
             event.msg.reply(PY_CODE_BLOCK.format(result))
-
-    def ProcessControl(self, signalNumber=None, frame=None):
-        if signalNumber == 2:
-            self.log.warning("Graceful shutdown initiated")
-            self.client.gw.shutting_down = True
-            self.client.gw.ws.close(status=4000)
-            self.client.gw.ws_event.set()
-            # unload plugins
-            for x in list(self.bot.plugins):
-                if x == 'CorePlugin':
-                    self.log.info('Skiping plugin: {}'.format(x))
-                    continue
-                plugin = next((v for k, v in self.bot.plugins.items() if k.lower() == x.lower()), None)
-                if plugin:
-                    self.log.info('Unloading plugin: {}'.format(x))
-                    try:
-                        self.bot.rmv_plugin(plugin)
-                    except BlockingSwitchOutError:
-                        self.log.warning("Plugin {} Has a active greenlet/schedule, Bruteforce".format(x))
-                        # Due to the never ending disco bugs, Just remove the listeners to stop plugin execution after unload
-                        plugin.greenlet = []
-                        plugin.schedule = []
-                        for listener in plugin.listeners:
-                            listener.remove()
-                        pass
-                    except Exception:
-                        self.log.exception("Failed to unload: {}".format(x))
-        elif signalNumber == 10:  # sysuser1
-            self.log.warning("Reseting shard connection to Discord")
-            self.client.gw.ws.close(status=4000)
-        else:
-            self.log.warning("Force Shutdown initiated")
-            os.kill(os.getpid(), signal.SIGKILL)
