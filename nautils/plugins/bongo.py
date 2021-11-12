81dc8c8ac181af82e4a9c2fe6bc73af968b1fcb1
Nadie <hackerjef@users.noreply.github.com>
2021-11-11 22:16:35 -0500

    owo

--- nautils/plugins/bongo.py
+++ nautils/plugins/bongo.py
@@ -24,7 +24,7 @@ class bongoPlugin(naPlugin):
         if event.author.id in Getcfgvalue('options.bongo.exception', []):
             return
 
-        if "content" not in event:
+        if "content" not in dir(event):
             return event.delete()
 
         # if something
