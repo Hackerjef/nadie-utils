dca6da2303dc3e31ac0436d9f0cc42fbfa1d8175
Nadie <hackerjef@users.noreply.github.com>
2021-11-12 18:13:41 -0500

    death

--- nautils/routes/root.py
+++ nautils/routes/root.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, redirect, request, send_from_directory
 
-join = Blueprint('root', __name__, url_prefix='/')
+root = Blueprint('root', __name__, url_prefix='/')
 
 
-@join.route('/')
+@root.route('/')
 def root():
     if "discord" in str(request.headers.get('User-Agent')).lower():
         return send_from_directory('nautils/www/dmeta.html')
