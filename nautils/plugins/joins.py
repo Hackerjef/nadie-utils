aac05c0e91eb1418a5cb588695127ff959aa2f83
Nadie <hackerjef@users.noreply.github.com>
2021-11-13 21:47:07 -0500

    a

--- nautils/plugins/joins.py
+++ nautils/plugins/joins.py
@@ -1,3 +1,5 @@
+import gevent
+
 from nautils import naPlugin
 from nautils.config import Getcfgvalue
 
@@ -10,19 +12,22 @@ class joinPlugin(naPlugin):
     def joins_ready(self, event):
         gid = Getcfgvalue("options.gid", None)
         if gid:
-            g = self.bot.client.state.guilds.get(gid)
-            if g:
-                for mid in list(g.members):
-                    if mid not in Getcfgvalue("options.joins.soft_banned", []):
-                        res = f"{mid} - User has been soft banned :) - bot startup"
-                        self.log.info(res)
-                        try:
-                            #g.kick(mid, reason=res)
-                            pass
-                        except:
-                            self.log.info(f"Can't kick user - {mid}")
-            else:
-                self.log.error("Guild not found for joins")
+            has_ran = False
+            while not has_ran:
+                g = self.bot.client.state.guilds.get(gid)
+                if g:
+                    for mid in list(g.members):
+                        if mid not in Getcfgvalue("options.joins.soft_banned", []):
+                            res = f"{mid} - User has been soft banned :) - bot startup"
+                            self.log.info(res)
+                            try:
+                                #g.kick(mid, reason=res)
+                                pass
+                            except:
+                                self.log.info(f"Can't kick user - {mid}")
+                else:
+                    self.log.error("Guild not found for joins, sleeping 15")
+                    gevent.sleep(15)
 
     @naPlugin.listen('GuildMemberAdd')
     def joins_onjoin(self, event):
