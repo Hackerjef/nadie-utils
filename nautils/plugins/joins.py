70c4442937586a967fbd2c16519bf0121cc24c4d
Nadie <hackerjef@users.noreply.github.com>
2021-11-13 21:39:35 -0500

    a

--- nautils/plugins/joins.py
+++ nautils/plugins/joins.py
@@ -2,22 +2,20 @@ from nautils import naPlugin
 from nautils.config import Getcfgvalue
 
 
-def check_guild(g):
-    for m in list(g.members):
-        print(m)
-        # if m in Getcfgvalue("options.joins.soft_banned", []):
-
-
-
-
 class joinPlugin(naPlugin):
     def load(self, ctx):
         super(joinPlugin, self).load(ctx)
 
     @naPlugin.listen("Ready")
     def joins_ready(self, event):
-        g = Getcfgvalue("options.gid", None)
-        self.log.info(self.bot.client.state.guilds)
+        gid = Getcfgvalue("options.gid", None)
+        if gid:
+            g = self.bot.client.state.guilds.get(gid)
+            if g:
+                for mid in list(g.members):
+                    print(mid)
+            else:
+                self.log.error("Guild not found for joins")
 
     @naPlugin.listen('GuildMemberAdd')
     def joins_onjoin(self, event):
